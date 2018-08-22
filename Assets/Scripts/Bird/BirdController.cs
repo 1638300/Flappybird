1b25dd7ccd609019b3913fb5be7a122ed101a199
unknown <1638300@cegep-ste-foy.qc.ca>
2018-08-22 16:56:36 -0400

    Configured the project.

---
+++ Assets/Scripts/Bird/BirdController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+  [SerializeField] private KeyCode flapKey = KeyCode.Space;
+  BirdPhysics birdPhysics = null;
+
+  // Use this for initialization
+  void Awake () 
+  {
+    birdPhysics = transform.root.GetComponentInChildren<BirdPhysics>();
+  }
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    if (Input.GetKeyDown(flapKey))
+      birdPhysics.Flap();
+
+  }
+}
---
+++ Assets/Scripts/Pipe/TimedDestroy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour 
+{
+  [SerializeField] private float destroyDelayInSeconds = 20f;
+
+  private void OnEnable()
+  {
+    StartCoroutine(DestroyRoot());
+  }
+  private void OnDisable()
+  {
+    StopAllCoroutines();
+  }
+
+  private IEnumerator DestroyRoot()
+  {
+    while (true)
+    {
+      yield return new WaitForSeconds(destroyDelayInSeconds);
+      DeletePrefab();
+    }
+  }
+
+  private void DeletePrefab()
+  {
+    Destroy(transform.root.gameObject);
+  }
+}
